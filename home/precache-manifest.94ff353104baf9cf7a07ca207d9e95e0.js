2eba7c39f6955ac70e3ac57715d8701b274a300d
Alongkon <alongkoninsuwan@gmail.com>
2019-11-20 22:04:03 +0700

    build

---
+++ home/precache-manifest.94ff353104baf9cf7a07ca207d9e95e0.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbdac951a174d440ba676c7b6febae2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c4772372df677e338ec2.bundle.js"
+  },
+  {
+    "url": "runtime~main.c4772372df677e338ec2.bundle.js"
+  },
+  {
+    "revision": "efe01b90316cc1eca182c240aa38b1f8",
+    "url": "static/media/employee1.efe01b90.svg"
+  },
+  {
+    "revision": "28498c4f21bb57f28c29bd22682515b4",
+    "url": "static/media/employee2.28498c4f.svg"
+  },
+  {
+    "revision": "a01f134c77daa3dc2142062572dd69f2",
+    "url": "static/media/employee3.a01f134c.svg"
+  },
+  {
+    "revision": "adb49148a10b203ec8389a17f140643e",
+    "url": "static/media/good-face.adb49148.svg"
+  },
+  {
+    "revision": "013b08bc11673f51686845bbc6477879",
+    "url": "static/media/half_star.013b08bc.png"
+  },
+  {
+    "revision": "4a22ff1dbd0978db444934787b82922a",
+    "url": "static/media/star.4a22ff1d.png"
+  },
+  {
+    "url": "vendors~main.c4772372df677e338ec2.bundle.js"
+  },
+  {
+    "revision": "2d0dc68fc7a5224be939",
+    "url": "vendors~main.css"
+  }
+]);
